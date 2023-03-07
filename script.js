3e6f7bd423bdcc3e8650daf3a68663b4e7728955
Wojciech Miczolek <wojciechm2291@gmail.com>
2023-03-07 18:58:09 +0000

    Add new getComputerChoice and playRound functions

--- script.js
+++ script.js
@@ -1,62 +1,36 @@
-// computer makes a random choice
+// get a random choice from the computer
+// 0-rock, 1-paper, 2-scissors
+
 
 function getComputerChoice() {
-    let randomNumber = Math.random() * 3;
-    if (randomNumber <= 1) {
-        return "ROCK"
-    } else if (randomNumber > 1 && randomNumber <= 2) {
-        return "PAPER"
+    const choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+    return "rock"
+    } else if (choice == 1) {
+    return "paper"
     } else {
-        return "SCISSORS"
+    return "scissors"
     }
 }
 
-// user is asked to make their choice
+//console.log(getComputerChoice());
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Make your choice").toUpperCase();
-    return playerChoice;
+// a function that plays a single round and returns who won the round
+
+function playRound(playerSelection, computerSelection) {
+if ((playerSelection == computerSelection)) {
+    console.log("tie");
+} else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")) {
+    console.log("you won")
+} else {
+    console.log("you lost")
+}
 }
 
-// variables/parameters that will be used in playRound function
-
+const playerSelection = "rock"
 const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
 
-// function that takes computer and user inputs and shows who won
-
-function playRound(computerSelection, playerSelection) {
-    if (
-        (computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
-        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
-        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
-    ) {
-        return `You lost. ${computerSelection} beats ${playerSelection}`;
-    } else if (
-        (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
-        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
-    ) {
-        return `You won. ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection === computerSelection) {
-        return `It's a draw`;
-    }
-}
-
-console.log(playRound(computerSelection, playerSelection));
-
-//////////////////////
-
-let playerScore = 0;
-let computerScore = 0;
-
-function game() {
-    playRound(computerSelection, playerSelection);
-}
-
-for (let i = 0; i < 5; i++) {
-    game();
-
- }
-
-
+playRound(playerSelection, computerSelection);
